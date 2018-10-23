2c996a96a17a83ea768340b7d6c516e535fd52e5
jstrickler <jstrickler@gmail.com>
2018-10-23 08:49:44 -0400

    Tuesday AM

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,1 +1,54 @@
 #!/usr/bin/env python
+
+fruits = ["pomegranate", "cherry", "apricot", "date", "apple",
+"lemon", "kiwi", "orange", "lime", "watermelon", "guava",
+"papaya", "fig", "pear", "banana", "tamarind", "persimmon",
+"elderberry", "peach", "blueberry", "lychee", "grape" ]
+
+
+f0 = []
+for f in fruits:
+    if f.startswith('p'):
+        f0.append(f.upper())
+print("f0:", f0, '\n')
+
+f1 = [f.upper() for f in fruits]
+print("f1:", f1, '\n')
+
+f2 = [f.upper() for f in fruits if f.startswith('p')]
+print("f2:", f2, '\n')
+
+f3 = [f for f in fruits if f.startswith('p')]
+print("f3:", f3, '\n')
+
+nums = [800, 80, 1000, 32, 255, 400, 5, 5000]
+
+n1 = [float(i * 2) for i in nums]
+print("n1:", n1, '\n')
+
+
+people = [
+    ('Melinda', 'Gates', 'Gates Foundation'),
+    ('Steve', 'Jobs', 'Apple'),
+    ('Larry', 'Wall', 'Perl'),
+    ('Paul', 'Allen', 'Microsoft'),
+    ('Larry', 'Ellison', 'Oracle'),
+    ('Bill', 'Gates', 'Microsoft'),
+    ('Mark', 'Zuckerberg', 'Facebook'),
+    ('Sergey','Brin', 'Google'),
+    ('Larry', 'Page', 'Google'),
+    ('Linus', 'Torvalds', 'Linux'),
+]
+
+last_names = [p[1] for p in people]
+print("last names:", last_names, '\n')
+
+unique_last_names = {p[1] for p in people}
+print("unique last names:", unique_last_names, '\n')
+
+g1 = (f.upper() for f in fruits)
+print(g1, '\n')
+# print(list(g1))
+for fruit in g1:
+    print(fruit)
+print()
