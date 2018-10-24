5dbc3b9ddf2b7c9abaa367692d574adf1ec83103
jstrickler <jstrickler@gmail.com>
2018-10-24 10:19:04 -0400

    XML stuff

---
+++ xml_book_tags.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# (c) 2016 John Strickler
+#
+from lxml.builder import ElementMaker
+
+_NS_BOOK_URL = "http://www.cja-tech.com/ns/books"
+
+_E = ElementMaker(
+    namespace=_NS_BOOK_URL,
+    nsmap={'bk': _NS_BOOK_URL}
+)
+
+BOOK = _E.book
+BOOK_TITLE = _E.title
+BOOK_AUTHOR = _E.author
+BOOK_PUB = _E.publisher
+BOOK_PUB_YEAR = _E.pubyear
